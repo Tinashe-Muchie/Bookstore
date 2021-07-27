9fb743c7641fa1b2a020630fa78479105d83d1c2
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-07-27 20:06:58 +0200

    initialize Accounting component

---
+++ src/Components/Accounting/Accounting.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { Context } from '../../Context/Context';
+
+function Accounting() {
+
+    //Retrieve products information from context
+    const { Products, Commerce, dispatch } = useContext(Context);
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Accounting
