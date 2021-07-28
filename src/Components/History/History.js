484d6f59e3ca70568fe354f8a54b18e4094b8ea2
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-07-28 11:07:19 +0200

    initialize HIstory component

---
+++ src/Components/History/History.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import { Context } from '../../Context/Context';
+
+function History() {
+
+    const { Products, Commerce, dispatch } = useContext(Context);
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default History;
