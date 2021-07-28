2feca62c51db08e23a76f92e2f0d53098d2fb8f4
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-07-28 11:08:27 +0200

    initialize Ecommerce component

---
+++ src/Components/E-Commerce/Ecommerce.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import { Context } from '../../Context/Context';
+
+function Ecommerce() {
+
+    const { Products, Commerce, dispatch } = useContext(Context);
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Ecommerce
