75b68d804f02efb54816cf3a21e27e22df748abd
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-08-07 13:29:28 +0200

    book category data

---
+++ src/Components/CategoryData.js
@@ -0,0 +1,27 @@
+
+export const CategoryData = [
+    {
+        title: 'Home',
+        link: '/'
+    },
+    {
+        title: 'Accounting',
+        link: '/accounting'
+    },
+    {
+        title: 'Management',
+        link: '/management'
+    },
+    {
+        title: 'Algorithms',
+        link: '/algorithms'
+    },
+    {
+        title: 'E Commerce',
+        link: '/ecommerce'
+    },
+    {
+        title: 'History',
+        link: '/history'
+    }
+];
