cb0a10c731b99a59d30e05c72050ea0df97722e7
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-08-16 20:10:53 +0200

    responsive styles modifications

--- src/Pages/Management.js
+++ src/Pages/Management.js
@@ -1,29 +1,78 @@
 import React, { useContext } from 'react';
+import styled from 'styled-components';
 import { GlobalContext } from '../Context/Context';
-import { Container, Row } from 'react-bootstrap';
-/*import the Layout from components*/
-import ProductLayout from '../Components/ProductLayout';
+import { device } from '../Components/index';
+import ProductLayout from '../Components/ProductLayout/ProductLayout';
 
 function Management() {
 
-    const { Categories, Commerce, dispatch} = useContext(GlobalContext);
+    const { Categories } = useContext(GlobalContext);
     
     return (
-        <Container>
-            <Row>
-            {
+        <Wrapper>
+            <StyledUOL>
+                <GridContainer>
+                {
                 Categories?.management?.map( (book) => (
                     <ProductLayout 
-                    bookImg={book.media.source}
-                    bookPrice={book.price.formatted_with_symbol}
-                    bookId={book.id}
-                    category='management'
-                    />
-                ))
-            }
-            </Row>       
-        </Container>
+                        bookId={book.id}
+                        bookImg={book.media.source}
+                        bookPrice={book.price.formatted_with_symbol}
+                        bookName={book.name}
+                        bookDescription={book.description}
+                        relatedBooks={book.related_products}
+                    />   
+                    ))
+                }
+                </GridContainer>
+            </StyledUOL>       
+        </Wrapper>
     );
 }
 
 export default Management;
+
+/*Styling using styled components*/
+export const Wrapper = styled.div`
+    margin: 0rem 0rem;
+
+    @media ${device.tablet} {
+        margin: 0vh 10vw;
+        padding: 2vh 0vw;
+    }
+`;
+
+ export const StyledUOL = styled.ul`
+    padding: 0px 10px;
+`;
+
+export const GridContainer =styled.div`
+    display: grid;
+    grid-template-columns: repeat(1, .5fr);
+    gap: 1rem 0.1rem;
+    justify-content: center;
+    margin: .5rem 0rem;
+
+    @media ${device.mobileM} {
+        grid-template-columns: repeat(2, .5fr);
+        gap: 1rem 0.1rem;
+        justify-items: center;
+    }
+
+    @media ${device.tablet} {
+        grid-template-columns: repeat(3, 2fr);
+        gap: 1.5rem .25rem;
+    }
+
+    @media ${device.laptop} {
+        grid-template-columns: repeat(5, 3fr);
+        gap: 1rem .75rem;
+    }
+
+    @media ${device.desktop} {
+        grid-template-columns: repeat(8, 3fr);
+        gap: 1rem .75rem;
+    }
+`;
+
+ 
